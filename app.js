e2fdd2e7c8eefd2b9360ac5d35e464b1ad4579ad
umaicode <park.seungchan99@gmail.com>
2024-10-09 19:03:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const COLORS = [
+    {r: 45, g: 74, b: 227}, // blue
+    {r: 250, g: 255, b: 89}, // yellow
+    {r: 255, g: 104, b: 248}, // pupple
+    {r: 44, g: 209, b: 252}, // skyblue
+    {r: 54, g: 233, b: 84} // green
+];
+
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        this.totalParticles = 1;
+        this.particles = [];
+        this.maxRadius = 90;
+        this.minRadius = 40;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.createParticles();
+    }
+
+    createParticles() {
+        let curColor = 0;
+        this.particles = [];
+
+        for (let i = 0; i < this.totalParticles; i++) {
+            const item = new Particle(
+                Math.random() * this.stageWidth,
+                Math.random() * this.stageHeight,
+                Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
+                COLORS[curColor]
+            );
+
+            if (++curColor >= COLORS.length) {
+                curColor = 0;
+            }
+
+            this.particles[i] = item;
+        }
+    }
+}
+
+window.onload = () => {
+    new App();
+}
